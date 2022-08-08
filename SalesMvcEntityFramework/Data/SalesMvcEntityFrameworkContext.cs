e89f70277c7390ccd17adade90b795d9d22462cc
iagolucas2020 <iago0905@hotmail.com>
2022-08-08 09:22:57 -0300

    First commit

---
+++ SalesMvcEntityFramework/Data/SalesMvcEntityFrameworkContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesMvcEntityFramework.Models
+{
+    public class SalesMvcEntityFrameworkContext : DbContext
+    {
+        public SalesMvcEntityFrameworkContext (DbContextOptions<SalesMvcEntityFrameworkContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Departament> Departament { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
