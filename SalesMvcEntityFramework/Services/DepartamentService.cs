4780d32879f238b710d6db697689b62cc0e8b865
iagolucas2020 <iago0905@hotmail.com>
2022-08-09 10:04:26 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesMvcEntityFramework/Services/DepartamentService.cs
+++ SalesMvcEntityFramework/Services/DepartamentService.cs
@@ -1,7 +1,8 @@
 ﻿using SalesMvcEntityFramework.Models;
-using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SalesMvcEntityFramework.Services
 {
@@ -14,9 +15,9 @@ namespace SalesMvcEntityFramework.Services
             _context = context;
         }
 
-        public List<Departament> FindAll()
+        public async Task<List<Departament>> FindAllAsync()
         {
-            return _context.Departament.OrderBy(x => x.Name).ToList(); ;
+            return await _context.Departament.OrderBy(x => x.Name).ToListAsync(); ;
         }
     }
 }
--- SalesMvcEntityFramework/Services/SellerService.cs
+++ SalesMvcEntityFramework/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using SalesMvcEntityFramework.Services.Exceptions;
+using System.Threading.Tasks;
 
 namespace SalesMvcEntityFramework.Services
 {
@@ -15,39 +16,40 @@ namespace SalesMvcEntityFramework.Services
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public async Task<List<Seller>> FindAllAsync()
         {
-            return _context.Seller.ToList();
+            return await _context.Seller.ToListAsync();
         }
 
-        public void Insert(Seller obj)
+        public async Task InsertAsync(Seller obj)
         {
-            _context.Add(obj); //Gravar no banco
-            _context.SaveChanges(); //Confirmar operação no banco
+            _context.Add(obj); //Grava em memória
+            await _context.SaveChangesAsync(); //Acessa o banco confirmar operação no banco
         }
 
-        public Seller FindById(int id)
+        public async Task<Seller> FindByIdAsync(int id)
         {
-            return _context.Seller.Include(obj => obj.Departament).FirstOrDefault(obj => obj.Id == id);
+            return await _context.Seller.Include(obj => obj.Departament).FirstOrDefaultAsync(obj => obj.Id == id);
         }
 
-        public void Remove(int id)
+        public async Task RemoveAsync(int id)
         {
-            var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void Update(Seller obj)
+        public async Task UpdateAsync(Seller obj)
         {
-            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny)
             {
                 throw new NotFoundExceptions("Id NotFound");
             }
             try
             {
                 _context.Update(obj);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException e)
             {
